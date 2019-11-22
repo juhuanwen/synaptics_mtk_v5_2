0d8ed9f23eb6b11ba3bb3dc72187e4f9f9fbbbac
Administrator <admin@example.com>
2019-11-22 16:09:41 +0800

    修改了触摸屏宽度为800*480

--- ilitek_ts.h
+++ ilitek_ts.h
@@ -92,7 +92,7 @@
 #define ILITEK_REVERT_X												0
 #define ILITEK_REVERT_Y												0
 #define TOUCH_SCREEN_X_MAX   										(800)	//LCD_WIDTH
-#define TOUCH_SCREEN_Y_MAX   										(400)	//LCD_HEIGHT
+#define TOUCH_SCREEN_Y_MAX   										(480)	//LCD_HEIGHT
 #define ILITEK_RESOLUTION_MAX										16384
 #define ILITEK_ENABLE_REGULATOR_POWER_ON
 #define ILITEK_GET_GPIO_NUM
